e5121f46535659f0f2b015d4028db8e30dfb3c28
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-18 08:37:16 -0500

    logic for component

---
+++ client/src/app/profile-forms/profile-forms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-forms.component.html',
+  styleUrl: './profile-forms.component.scss'
+})
+export class ProfileFormsComponent {
+
+}
