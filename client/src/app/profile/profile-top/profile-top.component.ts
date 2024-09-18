f8ae12e6790abaea0adf5f79f474de706d655f03
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-18 07:16:38 -0500

    imported common module, ngif and ngfor

--- client/src/app/profile/profile-top/profile-top.component.ts
+++ client/src/app/profile/profile-top/profile-top.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input  } from '@angular/core';
 import { Profile } from '../../models/profile';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'ng-profile-top',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule, NgFor],
   templateUrl: './profile-top.component.html',
   styleUrl: './profile-top.component.scss'
 })
 export class ProfileTopComponent {
+[x: string]: any;
   @Input()
   profile!: Profile;
   getSocialLinks() {
