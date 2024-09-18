4a54af1d40e343b8075edec7a4d8b482d0e6ca22
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-18 08:28:53 -0500

    logic for component

---
+++ client/src/app/profile/profile-education/profile-education.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-education',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-education.component.html',
+  styleUrl: './profile-education.component.scss'
+})
+export class ProfileEducationComponent {
+
+}
