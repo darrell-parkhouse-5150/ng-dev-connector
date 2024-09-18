c7d065b51ecfe1359ae32660b6d16230677eee9c
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-18 08:31:38 -0500

    logic for component

---
+++ client/src/app/profile/profile-experience/profile-experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-experience.component.html',
+  styleUrl: './profile-experience.component.scss'
+})
+export class ProfileExperienceComponent {
+
+}
