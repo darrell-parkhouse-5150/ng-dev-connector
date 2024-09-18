318edadc578d7e7d7fa2a24a01846d210fbc1759
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-18 08:33:22 -0500

    logic for component

---
+++ client/src/app/profile/profile-github/profile-github.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-github',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-github.component.html',
+  styleUrl: './profile-github.component.scss'
+})
+export class ProfileGithubComponent {
+
+}
