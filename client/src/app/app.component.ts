c3c16374f194663ae29d43855ceaf2462ae255b5
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-20 09:19:14 -0500

    imported the alert model

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { AlertComponent } from './alert/alert.component';
+import { AlertType } from './models/AlertTypes';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AlertComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
+  success!: AlertType;
 }
