280c5d09bd404a127cf188ddfeb3c73c63e9cdda
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-20 08:56:43 -0500

    defined the group invite table

--- server/database/dev-connector.sql
+++ server/database/dev-connector.sql
@@ -12,3 +12,27 @@ create table `users` (
     `updated_at` datetime,
     `created_at` datetime    
 );
+
+create table `online_users`(
+    `user_id` int(11) primary key auto_increment,
+    `login_time` time not null,
+    `user_name` varchar(40) not null
+);
+
+create table `groups` (
+    `grp_id` int (11) not null,
+    `grp_name` varchar(255) not null,
+    `grp_bio` text not null,
+    `grp_avatar` text not null,
+    `grp_admin` int(11) not null,
+    `grp_privacy` enum('public', 'private'),
+    `grp_created_at` datetime not null
+);
+
+create table `grp_invite`(
+    `grp_invite_id` int(11) primary key auto_increment,
+    `grp_name` int(11) not null,
+    `grp_inviter` int (11),
+    `grp_invitee` int (11),
+    `grp_time` datetime not null
+)
