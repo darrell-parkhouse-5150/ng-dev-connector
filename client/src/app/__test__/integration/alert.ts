314a7a17f75263f7fb4f045ef55e83fc679dc204
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-20 08:58:39 -0500

    wrote and integration test for the alert component

---
+++ client/src/app/__test__/integration/alert.ts
@@ -0,0 +1,58 @@
+import { TestBed } from '@angular/core/testing';
+import { AlertComponent } from '../alert/AlertComponent';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/alert.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AlertEffects } from './effects/alert.effects';
+
+describe('AlertComponent (integration)', () => {
+	let component: AlertComponent;
+	let store: Store;
+	let fixture: ComponentFixture<AlertComponent>;
+
+	beforeEach(async() => {
+		await TestBed.configureTestingModule({
+			imports: [
+				StoreModule.forRoot({ alert: reducer }),
+				EffectsModule.forRoot([AlertEffects])
+			],
+			declarations: [AlertComponent]
+		}).compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture: TestBed.createComponent(AlertComponent);
+		component: fixture.componentInstance;
+		store: TestBed.inject(Store);
+	});
+
+	it ('should display alert from store', () => {
+		store.dispatch({ type: '[Alert] Add Alert',
+			alert {
+				id: 1,
+				alertType: 'success',
+				msg: 'success message'
+			}
+		});
+
+		fixture.detectChanges();
+
+		const alertElem = fixture.nativeElement.querySelectorAll('.alert');
+
+		expect(alertElem.length).toBe(1);
+		expect(alertElem[0].textContent).toBe('success message')
+	});
+
+	it ('should update alerts when store changes', () => {
+		store.dispatch({ type: '[Alert] add alert', alert: { id: 1, alertType: 'success', msg: 'success message' }});
+		dispatch.detectChanges();
+		store.dispatch({ type: '[Alert] add alert', alert: { id: 2, alertType: 'error', msg: 'error message' }});
+		dispatch.detectChanges();
+
+		const alertElem = fixture.nativeElement.querySelectorAll('.alert');
+
+		expect(alertElem.length).toBe(2);
+		expect(alertElem[0].textContent).toBe('success message');
+		expect(alertElem[1].textContent).toBe('error message');
+	});
+});
