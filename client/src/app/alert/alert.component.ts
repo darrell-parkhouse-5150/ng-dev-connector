8f7b8d12978c25772c0e323b20369340a77271fd
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-20 09:17:44 -0500

    defined dismissAlert method()

--- client/src/app/alert/alert.component.ts
+++ client/src/app/alert/alert.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Alert } from '../models/alert';
+import { AlertType } from '../models/AlertTypes';
+import { Store } from '@ngrx/store';
+import { select, selectSnapshot } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { StoreModule } from '@ngrx/store';
 
 @Component({
-  selector: 'ng-alert',
-  standalone: true,
-  imports: [],
-  templateUrl: './alert.component.html',
-  styleUrl: './alert.component.scss'
+    selector: 'ng-alert',
+    standalone: true,
+    imports: [
+        StoreModule.forRoot({ alert: AlertReducer})
+    ],
+    templateUrl: './alert.component.html',
+    styleUrl: './alert.component.scss'
 })
 export class AlertComponent {
+    @Input() alerts$: Observable<Alert[]>;
 
+    constructor(private store: Store) {}
+
+    ngOnInit(): void {
+        this.alerts$ = this.store.select('alert');
+    }
+
+    // TODO: finish this function
+    dismissAlert(alert: Alert) {
+        this.store.dispatch()
+    }
 }
