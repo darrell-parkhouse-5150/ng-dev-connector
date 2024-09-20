f2d20e0cdcb53ad43103d3d3f5b5053dbb2ef6eb
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-20 09:05:25 -0500

    defined load, add, and remove actions for the alert component

--- client/src/app/actions/alert.actions.ts
+++ client/src/app/actions/alert.actions.ts
@@ -1,1 +1,25 @@
-alert.actions.ts
+import { createAction, props } from '@ngrx/store';
+import { Alert } from '../alert/alert.component.ts';
+
+/** 
+ * create action for all of the alerts:
+ * 	success,
+ * 	danger (error),
+ * 	warning,
+ *  info,
+ *
+ */
+export const LoadAlertSuccess = createAction(
+	'[Alert] Load Alerts Success',
+	props<{alerts: Alert[]}>()
+)
+
+export const AddAlert = createAction(
+	'[Alert] Add Alert',
+	props<{alert: Alert}>()
+)
+
+export const RemoveAlert = createAction(
+	'[Alert] Add Alert',
+	props<{id: string}>()
+)
