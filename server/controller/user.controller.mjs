3ab4571bd1b472a14dcda2fcc1f3d66b1b2f2712
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-22 08:24:03 -0500

    defined the login route handler

--- server/controller/user.controller.mjs
+++ server/controller/user.controller.mjs
@@ -18,7 +18,7 @@ const register = async (req, res) => {
 
         const results = await getResults(sql, [name, hashPass, email, avatar_url])
 
-        return res.status(203).send({
+        return res.status(201).send({
             message: 'successfully registered user',
             data: results
         })
@@ -31,7 +31,32 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    
+    try {
+        const { user_id, login_time, user_name } = req.body;
+
+        if (!user_id, !login_time, !user_name) {
+            return res.status(403).send({
+                message: 'all fields are required'
+            })
+        }
+
+        const sql = /*sql*/`
+            insert into online_users (user_id, login_time, user_name)
+            values (?, ?, ?)
+        `;
+
+        const results = await getResults(sql, [user_id, now(), user_name]);
+        
+        return res.status(201).send({
+            message: 'sauccessfully loged user in',
+            data: results
+        })
+    } catch (err) {
+        console.error(err);
+        return res.status(503).send({
+            message: 'internal server error'
+        })
+    }
 }
 
 module.exports = {
