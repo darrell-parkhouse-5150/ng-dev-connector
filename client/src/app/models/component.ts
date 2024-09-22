bc096f03d3a97b9e2dfa0aeac2c3444691dce093
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-22 15:32:35 -0500

    defined the component interface

---
+++ client/src/app/models/component.ts
@@ -0,0 +1,12 @@
+import { ComponentTypes } from "./ComponentTypes."
+import { User } from "./user"
+
+export interface Component {
+    id: number
+    name: string
+    author: User
+    version: string
+    type: ComponentTypes;
+    updated_at: Date
+    created_at: Date
+}
