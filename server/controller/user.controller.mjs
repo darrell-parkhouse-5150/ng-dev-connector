ac4795a9fae19886b0fe99c0d9d7eaeb6978f766
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-22 07:26:23 -0500

    defined the register user route handler

--- server/controller/user.controller.mjs
+++ server/controller/user.controller.mjs
@@ -1,7 +1,33 @@
 const bcrypt = require('bcrypt');
-
+import { getResults } from '../util/getResults'
 const register = async (req, res) => {
+    try {
+        const { name, pass, email, avatar_url } = req.body
+        if (!name || !pass || !email || avatar_url) {
+            return res.status(403).send({
+                message: 'all fields are required'
+            })
+        }
 
+        const hashPass = await bcrypt.hash(pass, 10)
+
+        const sql = /*sql*/`
+            insert into users (name, pass, email, avatar_url)
+            values (?, ?, ?, ?)
+        `;
+
+        const results = await getResults(sql, [name, hashPass, email, avatar_url])
+
+        return res.status(203).send({
+            message: 'successfully registered user',
+            data: results
+        })
+    } catch(err) {
+        console.error(err);
+        return res.status(503).send({
+            message: 'internal server error'
+        })
+    }
 }
 
 const login = async (req, res) => {
