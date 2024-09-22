0389b2444a01ad28b79fff26ff449e375438c107
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-22 14:09:58 -0500

    logic for the component

---
+++ client/src/app/posts/post-meta/post-meta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-meta',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-meta.component.html',
+  styleUrl: './post-meta.component.scss'
+})
+export class PostMetaComponent {
+
+}
