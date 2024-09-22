78cdfab01a1ab13c20fe4bcbfc8b46d3fa15e48b
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-22 08:50:39 -0500

    defined the getSingleUser, getAllUsers, updateUser, and removeUser routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,11 +3,21 @@ const router = express.Router()
 
 import {
     login,
-    register
+    register,
+    getAllUsers,
+    getSingleUser,
+    updateUser,
+    removeUser
 } from '../controller/user.controller.mjs'
 const baseUrl = 'http://localhost'
 
 router.post(`${baseUrl}/api/user/:user_id`, register);
 router.post(`${baseUrl}/api/user/login`, login);
+
+router.get(`${baseUrl}/api/`, getAllUsers);
+router.get(`${baseUrl}/api/user/:user_id`, getSingleUser);
+
+router.update(`${baseUrl}/api/user/:user_id`, updateUser)
+router.delete(`${baseUrl}/api/user/:user_id`, removeUser)
 
 module.exports = router
