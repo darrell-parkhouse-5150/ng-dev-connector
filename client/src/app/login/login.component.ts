63ed4fa82c92a9ba5d71712df59c0cfa768d1323
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-23 13:17:05 -0500

    changed the name of the selector

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'ng-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  
 
 }
