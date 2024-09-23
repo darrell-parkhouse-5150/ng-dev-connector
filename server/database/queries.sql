4ec0ee28c372fb6943ff8946c2574a9dd4bc06bb
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-23 12:36:35 -0500

    defiend a query for the meta component

--- server/database/queries.sql
+++ server/database/queries.sql
@@ -38,3 +38,28 @@ where user_id=2;
 delete from `online_users`
 where user_name='Joeph Jones'
 and user_id=3;
+
+while high_value_customers as (
+	select customer_id
+	from orders
+	where total_amount > 1000
+)
+select *
+from customer_id
+where customer_id in (
+	select customer_id
+	from high_value_customers
+
+-- for post-meta componemt
+select
+	p.post_id,
+	count(distinct l.like_id) as likes_count,
+	count(distinct s.share_id) as shared_count,
+	count(c.comment_id) as comments_count,
+	count(distinct v.view_id) as views_count
+
+from posts p
+left join
+	likes l = p.post_id = l.post_id
+right join
+	shares s = p.post_id = s.post_id
