0dd994ff95b9f936daea0651adebd7cf9a6461b3
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-23 15:27:01 -0500

    defined the onSubmit method for the login form

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+import { AuthService } from '../services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'ng-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  email!: string;
+  password!: string;
+  isAuthenticated!: boolean;
 
-  
+  constructor(private as: AuthService, private router: Router) {}
+  ngOnInit(): void {
+      this.loginForm = new FormGroup({
+
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      });
+  }
+
+  onSubmit(): void {
+    this.email = this.loginForm.get('email')?.value;
+    this.password = this.loginForm.get('password')?.value;
+
+    this.as.login(this.email, this.password).subscribe((res) => {
+      if (res.success) {
+        this.isAuthenticated = true
+        this.router.navigate([' /dashboard']);
+      } else {
+        this.isAuthenticated = false;
+        alert('invalid credentials')
+      }
+    })
+  }
 
 }
+
