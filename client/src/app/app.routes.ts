fe8d8e5645dc7f42a9001942b4a6efe96e5a366f
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-23 13:19:30 -0500

    defined the base path to load the home component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { AlertComponent } from './alert/alert.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: AlertComponent
+        component: HomeComponent
     }
 ];
