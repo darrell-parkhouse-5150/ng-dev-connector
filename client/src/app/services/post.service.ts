d4e58df042dd79f8cd73c20a29aee0cc907a75bd
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-27 09:38:06 -0500

    defined the addComment method

---
+++ client/src/app/services/post.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+   private apiUrl = 'http://localhost:3000/posts'
+
+  constructor(private http: HttpClient) {}
+
+  addComment(postId: number, text: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/${postId}/comments`, { text })
+  }
+}
