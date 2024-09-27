fdbf1d11523b606fd0250e4d783499a027d92397
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-27 09:45:54 -0500

    defined the ngOnDestory to unsubscibe from the subscription

--- client/src/app/alert/alert.component.ts
+++ client/src/app/alert/alert.component.ts
@@ -1,43 +1,29 @@
-// @ts-ignore
-import { Component, Inject, Input } from '@angular/core';
-import { Alert } from '../models/alert';
-import { AlertType } from '../models/AlertTypes';
-// @ts-ignore
-import { Store } from '@ngrx/store';
-// @ts-ignore
-import { select, selectSnapshot } from '@ngrx/store';
-import { Observable } from 'rxjs';
-// @ts-ignore
-import { StoreModule } from '@ngrx/store';
-import { CommonModule, NgFor } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Subscription } from 'rxjs';
+import { AlertService } from '../services/alert.service';
+import { CommonModule, NgIf, NgClass } from '@angular/common';
 
 @Component({
     selector: 'ng-alert',
     standalone: true,
-    imports: [/*StoreModule.forRoot({ alert: AlertReducer})*/ CommonModule, NgFor],
+    imports: [CommonModule, NgClass, NgIf],
     templateUrl: './alert.component.html',
     styleUrl: './alert.component.scss'
 })
-export class AlertComponent {
-    
-    @Input()
-    alerts$!: Observable<Alert[]>;
-    
-    alert: Alert = {
-        type: AlertType.success,
-        msg: '',
-    }
 
-    // @ts-ignore
-    constructor(@Inject('store') private store: Store) {}
+export class AlertComponent implements OnInit, OnDestroy {
+    msg: any;
+    subscription: Subscription = new Subscription()
+
+    constructor(private as: AlertService) {}
 
     ngOnInit(): void {
-        this.alerts$ = this.store.select('alert');
-        this.store = null
+        this.subscription = this.as.alertObservable.subscribe(msg => {
+            this.msg = msg;
+        });
     }
 
-    // TODO: finish this function
-    dismissAlert(alert: Alert) {
-        this.store.dispatch()
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe()
     }
 }
