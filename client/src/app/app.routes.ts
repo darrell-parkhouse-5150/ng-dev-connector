7aee9201bda61e85d34ad87c6a306185a066a82d
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-28 09:40:43 -0500

    replace the register route path to the home component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AlertComponent } from './alert/alert.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: RegisterComponent
     }
 ];
