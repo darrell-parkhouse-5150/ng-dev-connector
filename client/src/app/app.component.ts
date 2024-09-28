d3c94edbce34a20c6d5c07b0d9366ac491a6f4f6
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-28 09:46:21 -0500

    imported and declared RouterOutlet, RegisterComponent, HeaderComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertType } from './models/AlertTypes';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
