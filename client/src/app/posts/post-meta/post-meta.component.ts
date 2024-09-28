ed592e44398a6dbf3de61ef881b8e6641f28ed34
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-28 14:22:22 -0500

    changed the name of the selector

--- client/src/app/posts/post-meta/post-meta.component.ts
+++ client/src/app/posts/post-meta/post-meta.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-post-meta',
+  selector: 'ng-post-meta',
   standalone: true,
   imports: [],
   templateUrl: './post-meta.component.html',
