035041894abefa59bcd9b93efa6aa5657d935d91
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-28 14:42:29 -0500

    changed the name of the selector

---
+++ client/src/app/posts/angular-component/angular-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-angular-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './angular-component.component.html',
+  styleUrl: './angular-component.component.scss'
+})
+export class AngularComponentComponent {
+
+}
