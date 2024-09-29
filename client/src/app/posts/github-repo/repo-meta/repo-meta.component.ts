eb2d7a5bbcd5b52b79d03abcbd212e85fad81494
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-29 06:42:14 -0500

    logic for component

---
+++ client/src/app/posts/github-repo/repo-meta/repo-meta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-meta',
+  standalone: true,
+  imports: [],
+  templateUrl: './repo-meta.component.html',
+  styleUrl: './repo-meta.component.scss'
+})
+export class RepoMetaComponent {
+
+}
