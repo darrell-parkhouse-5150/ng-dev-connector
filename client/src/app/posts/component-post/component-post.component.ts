cbab9419f2d081af259fc1c089b09e36c8dc2f7a
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-29 06:45:37 -0500

    logic for component

---
+++ client/src/app/posts/component-post/component-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-post.component.html',
+  styleUrl: './component-post.component.scss'
+})
+export class ComponentPostComponent {
+
+}
