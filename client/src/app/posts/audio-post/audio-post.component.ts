0721904115b6f60c88d116435826068908f3fa74
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-29 06:43:39 -0500

    logic for component

---
+++ client/src/app/posts/audio-post/audio-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './audio-post.component.html',
+  styleUrl: './audio-post.component.scss'
+})
+export class AudioPostComponent {
+
+}
