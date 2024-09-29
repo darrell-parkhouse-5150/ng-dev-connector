3ea56819ee12336d3dadbd35a5b08d22fac00e17
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-29 06:54:07 -0500

    defined all of the function without a function body

---
+++ server/controller/profile.controller.js
@@ -0,0 +1,4 @@
+export const createProfile = async (req, res) => {}
+export const getUserProfile = async (req, res) => {}
+export const updateProfile = async(req, res) => {}
+export const deleteProfile = async(req, res) => {}
