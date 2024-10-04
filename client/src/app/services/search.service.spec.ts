cfd0ee1009d445ae5f487e99f4f3d9949070c0f5
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-04 18:24:31 -0500

    wrote a test function to make sure that a search should return an empty set

---
+++ client/src/app/services/search.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SearchService } from './search.service';
+
+describe('SearchService', () => {
+  let service: SearchService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SearchService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  it('should not return an empty result set', () => {
+    
+  })
+});
