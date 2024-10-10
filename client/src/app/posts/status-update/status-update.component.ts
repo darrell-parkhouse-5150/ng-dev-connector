69ebd96a3486830fef8df64c9c7f48c13afcb3fe
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-10 08:40:54 -0500

    defined the structure/logic/and styles for the component

--- client/src/app/posts/status-update/status-update.component.ts
+++ client/src/app/posts/status-update/status-update.component.ts
@@ -1,20 +1,57 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { StatusUpdateService } from '../../services/status-update.service';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../models/user';
+import { StatusUpdate } from '../../models/status-update';
+import { NgFor } from '@angular/common';
 @Component({
-  selector: 'ng-status-update',
-  standalone: true,
-  imports: [],
-  templateUrl: './status-update.component.html',
-  styleUrl: './status-update.component.scss'
+    selector: 'ng-status-update',
+    standalone: true,
+    imports: [NgFor],
+    templateUrl: './status-update.component.html',
+    styleUrl: './status-update.component.scss',
 })
-export class StatusUpdateComponent implements OnInit {
-  constructor(private http: HttpClient, private StatusUpdateService: StatusUpdateService) {}
-  ngOnInit(): void {
-      this.StatusUpdateService.getAllStatusUpdate()
-        .subscribe((data) => {
-          //? TODO: write the code for this by hand. no AI :(
-        })
-  }
+export class StatusUpdateComponent {
+    statusText: string = '';
+    userId!: number;
+    user: any;
+    friends: any[] = [];
+    statusUpdates: any = [];
+    status: string = '';
+
+    constructor(private http: HttpClient) {}
+
+    getUsers(user_id: number) {
+        this.http.get(`/api/user/${user_id}`).subscribe((data) => {
+            this.user = data;
+        });
+    }
+
+    getStatusUpdates(): void {
+        this.http.get(`/api/user/status-updates`).subscribe((data) => {
+            this.statusUpdates = data;
+        });
+    }
+
+    postStatus(): void {
+        const taggedUserIds = this.extractTaggedUserIds(this.statusText)
+        
+        this.http
+            .post('/api/user/staus-updates/', this.status)
+            .subscribe(() => {
+                this.statusText = '';
+                this.getStatusUpdates();
+            });
+    }
+
+    extractTaggedUserIds(text: string): number[] {
+        const taggeUdsernames = text.match(/@(\w+)/g) || [];
+        return taggeUdsernames
+            .map((username) => {
+                const friend = this.friends.find(
+                    (f) => f.username === username.substring(1)
+                );
+                return friend ? friend.id : null;
+            })
+            .filter((id) => id !== null);
+    }
 }
