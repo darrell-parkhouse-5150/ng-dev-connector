c398a0e1ed54a8dcd4ff379edeeb89ec1ecf48c4
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-10 10:33:38 -0500

    logic for component

---
+++ client/src/app/posts/status-update/status-update-meta/status-update-meta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-update-meta',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-update-meta.component.html',
+  styleUrl: './status-update-meta.component.scss'
+})
+export class StatusUpdateMetaComponent {
+
+}
