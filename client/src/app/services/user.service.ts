7215c0a07d2ad3637f1287440145badacf652cc4
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-20 08:37:21 -0500

    defined the base crud functions

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  createUser() {}
+  getAllUsers() {}
+  getSingleUser() {}
+  updateUser() {}
+  deleteUser() {}
+}
