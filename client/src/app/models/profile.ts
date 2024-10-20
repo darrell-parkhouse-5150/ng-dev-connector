a62a444d4fd1d221787f6de0edd541b8610a31f1
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-20 08:26:45 -0500

    mode the id optional for the profile interface

--- client/src/app/models/profile.ts
+++ client/src/app/models/profile.ts
@@ -3,13 +3,13 @@ import { Experience } from './experience';
 import { User } from './user';
 
 export interface Profile {
-    id: string;
-    user: User;
-    company: string
-    experience: Experience
-    education: Education
-    location: string
-    website: string
-    status: string
-    social: { [key: string]: string }
+    id?: string;
+    user?: User;
+    company?: string
+    experience?: string
+    education?: string
+    location?: string
+    website?: string
+    status?: string
+    // social?: { [key: string]: string }
 }
