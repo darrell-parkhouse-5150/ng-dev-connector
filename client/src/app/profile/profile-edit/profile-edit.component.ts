117815a19f6b6c76568245338406ae7e3aae98c9
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-23 12:17:02 -0500

    removed unneeded methods

--- client/src/app/profile/profile-edit/profile-edit.component.ts
+++ client/src/app/profile/profile-edit/profile-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Profile } from '../../models/profile';
 import { ProfileTopComponent } from '../profile-top/profile-top.component';
 import { ProfileAboutComponent } from '../profile-about/profile-about.component';
@@ -12,7 +12,7 @@ import { NgIf } from '@angular/common';
   templateUrl: './profile-edit.component.html',
   styleUrl: './profile-edit.component.scss'
 })
-export class ProfileEditComponent {
+export class ProfileEditComponent implements OnInit {
     @Input() loading: boolean = false
     profileObj: Profile = {};
     isAuthenticated: boolean = false
@@ -22,16 +22,51 @@ auth: any;
 
     constructor(private ps: ProfileService) {}
 
+    ngOnInit(): void {
+        if (this.isAuthenticated === true) {
+            this.initForm();
+            this.fetchProfile();
+        }
+    }
+    
+
     fetchProfile(): void {
         this.ps.getProfiles()
             .subscribe((profile: any) => {
                 this.profileObj = profile
+                this.updateFormWithData(profile)
             }, (error: any) => {
-                console.error(error);
+                console.error('error fetching data', error);
             })
     }
 
     editProfile() {}
-    initForm() {}
+    initForm() {
+        this.editProfileForm = new FormGroup({
+            user: new FormControl(this.profileObj.user, [Validators.required]),
+            email: new FormControl(this.profileObj.email, [
+                Validators.required, 
+                Validators.email
+            ]),
+            experience: new FormControl(this.profileObj.experience, Validators.required),
+            education: new FormControl(this.profileObj.education, Validators.required),
+            location: new FormControl(this.profileObj.location, Validators.required),
+            status: new FormControl(this.profileObj.status, Validators.required),
+            social: new FormControl(this.profileObj.social, Validators.required)
+        })
+    }
     goBack() {}
+
+    updateFormWithData(profile: Profile) {
+        this.editProfileForm.patchValue({
+            user: profile.user,
+            email: profile.email,
+            experience: profile.experience,
+            education: profile.education,
+            location: profile.location,
+            website: profile.website,
+            status: profile.status,
+            social: profile.social
+        })
+    }
 }
